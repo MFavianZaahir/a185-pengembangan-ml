da4086bfea25d4c7934db56585cb96ebc261ea3c
MFavianZaahir <favianzaahir@gmail.com>
2025-03-05 14:26:48 +0700

    erm andro

--- sample_project_tflite/starter/SimpleImageClassification/app/src/main/java/com/example/imageclassification/TFLiteHelper.java
+++ sample_project_tflite/starter/SimpleImageClassification/app/src/main/java/com/example/imageclassification/TFLiteHelper.java
@@ -52,32 +52,110 @@ public class TFLiteHelper {
 
     // ---- Kolom inisiasi TensorFlow Lite Interpreter ----
 
+    void init() {
+        try {
+            Interpreter.Options opt = new Interpreter.Options();
+            tflite = new Interpreter(loadmodelfile(context), opt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     // ----------------------------------------------------
 
 
     // ---- Kolom preprocessing gambar ----
 
+    private TensorImage loadImage(final Bitmap bitmap) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+ 
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+ 
+    private TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }    
 
     // ----------------------------------------------------
 
     
     // ---- Kolom pemanggilan model tflite ----
 
+    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
+        String MODEL_NAME = "vegs.tflite";
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_NAME);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startoffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startoffset, declaredLength);
+    }
 
     // ----------------------------------------------------
     
     
     // ---- Kolom klasifikasi ----
 
+    void classifyImage(Bitmap bitmap){
+        int imageTensorIndex = 0;
+        int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+        imageSizeY = imageShape[1];
+        imageSizeX = imageShape[2];
+        DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+ 
+        int probabilityTensorIndex = 0;
+        int[] probabilityShape =
+                tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+        DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+ 
+        inputImageBuffer = new TensorImage(imageDataType);
+        outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+        probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+ 
+        inputImageBuffer = loadImage(bitmap);
+ 
+        tflite.run(inputImageBuffer.getBuffer(),outputProbabilityBuffer.getBuffer().rewind());
+    }
 
     // ----------------------------------------------------
 
 
     // ---- Kolom postprocessing ----
 
+    public String showresult() {
+        try {
+            labels = FileUtil.loadLabels(context, "vegs.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        Map<String, Float> labeledProbability =
+                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+                        .getMapWithFloatValue();
+        float maxValueInMap = (Collections.max(labeledProbability.values()));
+        String result = null;
+        for (Map.Entry<String, Float> entry : labeledProbability.entrySet()) {
+            if (entry.getValue() == maxValueInMap) {
+                result = entry.getKey();
+            }
+        }
+ 
+        return result;
+    }
+ 
+    private TensorOperator getPostprocessNormalizeOp() {
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+    }
 
     // ----------------------------------------------------
 
 }
-
